4ba70261c380fc8d12e35ce8fec683c416309080
Nick Land <land.d.nicholas@gmail.com>
2025-06-22 16:36:33 +0000

    docs(app): add storyfile

---
+++ src/App.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import App from './App';
+
+const meta = {
+  component: App,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof App>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
